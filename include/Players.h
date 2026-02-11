4c30aacba05078e4d1ca1b03a0c8f5356b5917dd
Sebastian <SebastianMOlesen@Gmail.com>
2026-02-11 16:32:17 +0100

    Cleaned up the Player source and header file

--- include/Players.h
+++ include/Players.h
@@ -1,33 +1,28 @@
 #pragma once
-#include "SFML/System/Vector2.hpp"
 #include <SFML/Graphics.hpp>
 
-struct PlayerParams
-{
-    sf::Vector2f viewportSize;
-    float width{20};
-    float height{80};
-    float clearance{20};
-    bool playerOne{true};
-    bool AI{false};
+struct PlayerParams {
+        sf::Vector2f viewportSize;
+        sf::Vector2f size{20, 80};
+        bool playerOne{true};
+        bool AI{false};
 };
 
-class Player : public sf::RectangleShape
-{
-  public:
-    Player() = default;
-    Player(const PlayerParams &config);
-    ~Player() = default;
+class Player : public sf::RectangleShape {
+       public:
+        Player() = default;
+        Player(const PlayerParams& config);
+        ~Player() = default;
 
-    void Update(float deltaTime, sf::Vector2f ballPos);
+        void Update(float deltaTime, sf::Vector2f ballPos);
 
-  private:
-    sf::Keyboard::Key m_UpKey;
-    sf::Keyboard::Key m_DownKey;
-    sf::Vector2f m_ViewportSize;
-    float m_Speed;
-    bool m_IsAI;
+       private:
+        sf::Keyboard::Key m_UpKey;
+        sf::Keyboard::Key m_DownKey;
+        sf::Vector2f m_ViewportSize;
+        float m_Speed;
+        bool m_IsAI;
 
-    void Move(float deltaTime);
-    void AIMove(float deltaTime, sf::Vector2f ballPos);
+        void Move(float deltaTime);
+        void AIMove(float deltaTime, sf::Vector2f ballPos);
 };
